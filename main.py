f3c7837548ce977fd6efe0eeb9e24f7a111fe9b0
Manuel Vujakovic <manuelvujak@gmail.com>
2022-11-13 14:34:31 +0100

    Made code pretty

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-#import pandas as pd
+import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-
-#kv6loraw3A6MdnXd never gonna give you up
-
-#metadata = pd.read_csv("C:\Uni\2022W\MMSR\id_information_mmsr.tsv", sep="\t")
-#tf_idf = pd.read_csv("C:\Uni\2022W\MMSR\id_lyrics_tf-idf_mmsr.tsv", sep="\t")
-
-#print(metadata.where(metadata['id'] == 'kv6loraw3A6MdnXd'))
-#print(metadata[where 'id' == 'kv6loraw3A6MdnXd']))
-
-#print(metadata)
-#print(cosine_similarity(metadata, tsv_file))
-
 import csv
 
-
 if __name__ == "__main__":
+
     sought_line = 0
+    input_query = "kv6loraw3A6MdnXd"
 
     with open("./resources/id_lyrics_tf-idf_mmsr.tsv") as file:
         tsv_file = csv.reader(file, delimiter="\t")
         for line in tsv_file:
-            if(line[0] == "kv6loraw3A6MdnXd"):
+            if(line[0] == input_query):
                 print(line)
                 sought_line = line
                 break
 
+    # region dead code
     #for line in tsv_file:
     #    print(cosine_similarity(np.array(sought_line[1:]), np.transpose(np.array(line[1:]))))
+
+
+
+        # kv6loraw3A6MdnXd never gonna give you up
+
+        # metadata = pd.read_csv("C:\Uni\2022W\MMSR\id_information_mmsr.tsv", sep="\t")
+        # tf_idf = pd.read_csv("C:\Uni\2022W\MMSR\id_lyrics_tf-idf_mmsr.tsv", sep="\t")
+
+        # print(metadata.where(metadata['id'] == 'kv6loraw3A6MdnXd'))
+        # print(metadata[where 'id' == 'kv6loraw3A6MdnXd']))
+
+        # print(metadata)
+        # print(cosine_similarity(metadata, tsv_file))
+    #endregion
