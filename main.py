297861810d999304bf390b9c0fb896c92808f1b7
Manuel Vujakovic <manuelvujak@gmail.com>
2022-11-13 17:05:09 +0100

    tfidf cosine  + bert euclidian working well :>

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
-import csv
 import collections
+import csv
+
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-
+from sklearn.metrics.pairwise import euclidean_distances
 
 if __name__ == "__main__":
 
-    sought_line = None
+    sought_line_cosine = None
+    sought_line_euclidean = None
     input_query = "kv6loraw3A6MdnXd"
+
 
     with open("./resources/id_lyrics_tf-idf_mmsr.tsv") as file:
         tsv_file = csv.reader(file, delimiter="\t")
-        # skip first line as is is the string headers
+        # skip first line as it is the string headers
         next(tsv_file)
         for line in tsv_file:
             if line[0] == input_query:
                 # Finding full line of our searched query
-                sought_line = line
+                sought_line_cosine = line
                 break
 
-    if sought_line is None:
+    with open("./resources/id_bert_mmsr.tsv") as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        # skip first line as it is the string headers
+        next(tsv_file)
+        for line in tsv_file:
+            if line[0] == input_query:
+                # Finding full line of our searched query
+                sought_line_bert = line
+                break
+
+    if sought_line_cosine is None or sought_line_bert is None:
         raise NameError("The searched input is not in the database")
 
+    # using tfidf
     cosines = dict()
+    # using bert
+    euclideans = dict()
 
     with open("./resources/id_lyrics_tf-idf_mmsr.tsv") as file:
         tsv_file = csv.reader(file, delimiter="\t")
-        # skip first line as is is the string headers
+        # skip first line as it is the string headers
         next(tsv_file)
         for line in tsv_file:
-            cosines[line[0]] = cosine_similarity(np.array([sought_line[1:]]), np.array([line[1:]]))[0, 0]
+            cosines[line[0]] = cosine_similarity(np.array([sought_line_cosine[1:]]), np.array([line[1:]]))[0, 0]
 
-    print(cosines)
+    with open("./resources/id_bert_mmsr.tsv") as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        # skip first line as it is the string headers
+        next(tsv_file)
+        for line in tsv_file:
+            euclideans[line[0]] = (1 / (1 + euclidean_distances(np.array([sought_line_bert[1:]]), np.array([line[1:]]), squared=True)))[0, 0]
 
-    # order cosines on value
+    # order dictionaries
     sorted_cosines = collections.OrderedDict(sorted(cosines.items(), key=lambda kv: kv[1], reverse=True))
-    print("\n-----------------------------------The stuff we want ----------------------------------------------\n")
-    print(sorted_cosines)
-    print(type(sorted_cosines))
+    sorted_euclideans = collections.OrderedDict(sorted(euclideans.items(), key=lambda kv: kv[1], reverse=True))
+
+
