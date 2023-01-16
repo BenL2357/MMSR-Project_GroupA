829171fded71a741831d718b8ad95a0cd0f2b4f1
Tobi <t.hinum@gmx.at>
2023-01-16 20:36:51 +0100

    Query for all songs added

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ if __name__ == "__main__":
 
     def sim_query(input_query: [str], feature_vector_1, feature_vector_2=None, feature_function_mode=0):
         if feature_function_mode == 1:
-            similarity = cosine_similarity(feature_vector_1.loc[input_query], feature_vector_1) * 0.5 + \
+            similarity = ((cosine_similarity(feature_vector_1.loc[input_query], feature_vector_1) + 1) * 0.5) * 0.5 + \
                          (1 / (1 + euclidean_distances(feature_vector_2.loc[input_query], feature_vector_2,
                                                        squared=True))) * 0.5
         elif feature_function_mode == 2 or feature_function_mode == 4:
-            similarity = cosine_similarity(feature_vector_1.loc[input_query], feature_vector_1) * 0.5 + \
-                         cosine_similarity(feature_vector_2.loc[input_query], feature_vector_2) * 0.5
+            similarity = ((cosine_similarity(feature_vector_1.loc[input_query], feature_vector_1) * 0.5 +
+                           cosine_similarity(feature_vector_2.loc[input_query], feature_vector_2) * 0.5) + 1) * 0.5
         elif feature_function_mode == 3:
-            similarity = cosine_similarity(feature_vector_1.loc[input_query], feature_vector_1)
+            similarity = (cosine_similarity(feature_vector_1.loc[input_query], feature_vector_1) + 1) * 0.5
         else:
             raise ValueError("Feature function mode is invalid!")
         return similarity
@@ -271,6 +271,69 @@ if __name__ == "__main__":
         return precision_sum / len(all_songs), mrr_sum / len(all_songs), ndcg_sum_10 / len(
             all_songs), ndcg_sum_100 / len(all_songs)
 
+    def merged_performance_metrics(tfidf_df, bert_df, genres, video_features_resnet_max, video_features_resnet_mean, mfcc_bow, spectral, spectral_contrast, spotify_data):
+        all_results = pd.DataFrame(index=genres.index, columns=(["results"])).fillna("").apply(list)
+        all_results.index.name = "id"
+
+        splitted_dataset = np.array_split(genres, 14)
+        for dataset in splitted_dataset[0:1]:
+            query_songs = dataset.index
+            results_lyrics = sim_query(query_songs, tfidf_df, bert_df, 1)
+            results_video = sim_query(query_songs, video_features_resnet_max, video_features_resnet_mean, 2)
+            results_audio_bow = sim_query(query_songs, mfcc_bow, None, 3)
+            results_audio_spectral = sim_query(query_songs, spectral, spectral_contrast, 4)
+
+            results = results_lyrics * 0.25 + results_video * 0.25 + results_audio_bow * 0.25 + results_audio_spectral * 0.25
+            for i in range(len(query_songs)):
+                results[i, i] = 0
+
+            res = pd.DataFrame(results.T, index=genres.index, columns=query_songs)
+            res.index.name = "id"
+
+            for col in res.columns:
+                res_largest = res[col].nlargest(100)
+                all_results.loc[col]["results"] = res_largest.index.tolist()
+
+        index_loop = 0
+        delta_mean_array = np.zeros(len(genres))
+        precision_sum = 0
+        mrr_sum = 0
+        ndcg_sum_10 = 0
+        ndcg_sum_100 = 0
+
+        precision_arr_sum = 0
+        recall_arr_sum = 0
+
+        for index, result in all_results.iterrows():
+            if not result.empty:
+                delta_mean_array[index_loop] = percent_delta_mean(spotify_data, index, result["results"])
+
+                precision_arr, recall_arr = precision_recall_plot(index, genres, result["results"])
+
+                precision_arr_sum = precision_arr_sum + precision_arr
+                recall_arr_sum = recall_arr_sum + recall_arr
+
+                precision_sum += precision_s(genres, index, result["results"])
+                mrr_sum += mrr_s(genres, index, result["results"])
+                ndcg_sum_10 += nDCG_ms(result["results"], genres, index, 10)
+                ndcg_sum_100 += nDCG_ms(result["results"], genres, index, 100)
+                index_loop += 1
+
+        precision_arr_norm = precision_arr_sum / len(genres)
+        recall_arr_norm = recall_arr_sum / len(genres)
+
+        plot_precision_recall(precision_arr_norm, recall_arr_norm, "All songs")
+
+        print(f"Performan Metrics for All songs")
+        print(f"Precision: {precision_sum / len(genres)}\n")
+        print(f"MRR: {mrr_sum / len(genres)}\n")
+        print(f"NDCG10: {ndcg_sum_10 / len(genres)}\n")
+        print(f"NDCG100: {ndcg_sum_100 / len(genres)}\n")
+        print(f"Median Delta Mean: {np.median(delta_mean_array)}")
+
+
+
+
 
     def kendall_tau_correlation_calculation(tfidf_df, bert_df, video_features_resnet_max, video_features_resnet_mean,
                                             mfcc_bow, spectral, spectral_contrast, song_count=1000):
@@ -364,9 +427,10 @@ if __name__ == "__main__":
               f"Genre frequency to genre count: {genre_dict_genre_frequency}\n"
               f"Average genres per song: {average_genres_song:.4f}\n")
         # performance_metrics_s_baseline(tfidf_df, genres)
-        performance_metrics(tfidf_df, bert_df, 1, genres, spotify_data, 10000, thv=0.55)
-        performance_metrics(video_features_resnet_max, video_features_resnet_mean, 2, genres, spotify_data, 10000)
-        performance_metrics(mfcc_bow, None, 3, genres, spotify_data, 10000)
-        performance_metrics(spectral, spectral_contrast, 4, genres, spotify_data, 10000)
+        #performance_metrics(tfidf_df, bert_df, 1, genres, spotify_data, 10000, thv=0.55)
+        #performance_metrics(video_features_resnet_max, video_features_resnet_mean, 2, genres, spotify_data, 10000)
+        #performance_metrics(mfcc_bow, None, 3, genres, spotify_data, 10000)
+        #performance_metrics(spectral, spectral_contrast, 4, genres, spotify_data, 10000)
+        merged_performance_metrics(tfidf_df, bert_df, genres, video_features_resnet_max, video_features_resnet_mean, mfcc_bow, spectral, spectral_contrast, spotify_data)
         kendall_tau_correlation_calculation(tfidf_df, bert_df, video_features_resnet_max, video_features_resnet_mean,
                                             mfcc_bow, spectral, spectral_contrast)
