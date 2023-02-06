adca858a14ae62a22c978275eb0aa9da3b4a8b93
Manuel Vujakovic <manuelvujak@gmail.com>
2023-02-06 16:00:31 +0100

    small bugfix
    Fixed the code should work now

--- Generate_testing_sets.py
+++ Generate_testing_sets.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import os
-import os
+import random
 
 import pandas as pd
 
@@ -11,11 +10,17 @@ if __name__ == "__main__":
     if not os.path.exists(newfolder):
         os.makedirs(newfolder)
 
+    n = 68000
+    s = 5000
+
+    random.seed(123)
+    vals = sorted(random.sample(range(1, n + 1), s))
+
     for file in os.listdir():
 
         if file.endswith(".tsv"):
             temp = pd.read_csv(file, delimiter="\t", index_col="id").sort_values(
                 "id")
-            temp2 = temp.sample(5000, random_state=123)
+            temp2 = temp.iloc[vals, :]
             newfilename = os.path.join(newfolder, file)
-            temp2.to_csv(f"{newfilename}.tsv", sep="\t")
+            temp2.to_csv(newfilename, sep="\t")
